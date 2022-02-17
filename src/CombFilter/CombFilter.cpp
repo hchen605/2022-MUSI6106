79408dac98330dfa1cf0210cf5091e9307255d48
Hsin-Hung Chen <hsin-hung.chen@Hsin-HungdeMacBook-Pro.local>
2022-02-17 16:38:35 -0500

    add test1,2

--- src/CombFilter/CombFilter.cpp
+++ src/CombFilter/CombFilter.cpp
@@ -17,10 +17,9 @@ mfMaxDelayLengthInS(fMaxDelayLengthInS),
 ppRingBuffer(nullptr),
 mfSampleRateInHz(fSampleRateInHz)
 {
-    //setDelay(fMaxDelayLengthInS);
+    
     iMaxDelayInSample = static_cast<int>(mfMaxDelayLengthInS * mfSampleRateInHz);
-    //cout<<iMaxDelayInSample<<endl;
-    //cout<<mfMaxDelayLengthInS<<endl;
+    
     if (ppRingBuffer != nullptr) {
         delete ppRingBuffer;
     }
@@ -34,40 +33,15 @@ CCombFilterBase::~CCombFilterBase()
     delete ppRingBuffer;
 }
 
-/*
-Error_t CCombFilterBase::setGain(float fGain) {
-    
-    mfGain = fGain;
-    return Error_t::kNoError;
-}
-*/
-/*! Update the delay length of filter. This would cause deleting & newing CRingBuffer object.
-\param fDelay the delay time IN SECOND
-*/
-/*
-Error_t CCombFilterBase::setDelay(float fDelay) {
-    
-    mfDelay = fDelay;
-    iDelayInSample = static_cast<int>(mfDelay * mfSampleRateInHz);
-    if (ppRingBuffer != nullptr) {
-        delete ppRingBuffer;
-    }
-    ppRingBuffer = new CRingBuffer<float>*[iNumChannels];
-    for (int i=0; i<iNumChannels; ++i)
-        ppRingBuffer[i] = new CRingBuffer<float>(iDelayInSample);
-    return Error_t::kNoError;
-}
-*/
+
 
 Error_t CCombFilterBase::setParam(CCombFilterIf::FilterParam_t eParam, float fParamValue) {
     switch (eParam)
     {
     case CCombFilterIf::kParamGain:
-        //return setGain(fParamValue);
         mfGain = fParamValue;
         break;
     case CCombFilterIf::kParamDelay:
-        //return setDelay(fParamValue);
         mfDelay = fParamValue;
         iDelayInSample = static_cast<int>(mfDelay * mfSampleRateInHz);
         for (int i=0; i < iNumChannels; ++i)
@@ -90,10 +64,8 @@ float CCombFilterBase::getParam(CCombFilterIf::FilterParam_t eParam) const {
     switch (eParam)
     {
     case CCombFilterIf::kParamGain:
-        //return getGain();
         return mfGain;
     case CCombFilterIf::kParamDelay:
-        //return getDelay();
         return mfDelay;
     case CCombFilterIf::kNumFilterParams:
         return -1.F;
@@ -109,10 +81,7 @@ FIRCombFilter::FIRCombFilter(float fMaxDelayLengthInS, float fSampleRateInHz, in
 
 Error_t FIRCombFilter::process(float** ppfInputBuffer, float** ppfOutputBuffer, int iNumberOfFrames) {
     for (int i = 0; i < iNumberOfFrames; ++i) {
-        //ppRingBuffer->reset();
-        //ppRingBuffer->setWriteIdx(iDelayInSample);
         for (int j = 0; j < iNumChannels; ++j) {
-            //ppRingBuffer[j]->putPostInc(ppfInputBuffer[j][i]);
             ppfOutputBuffer[j][i] = ppfInputBuffer[j][i] + mfGain * ppRingBuffer[j]->getPostInc();
             ppRingBuffer[j]->putPostInc(ppfInputBuffer[j][i]);
         }
@@ -125,8 +94,6 @@ IIRCombFilter::IIRCombFilter(float fMaxDelayLengthInS, float fSampleRateInHz, in
 
 Error_t IIRCombFilter::process(float** ppfInputBuffer, float** ppfOutputBuffer, int iNumberOfFrames) {
     for (int i = 0; i < iNumberOfFrames; ++i) {
-        //ppRingBuffer->reset();
-        //ppRingBuffer->setWriteIdx(iDelayInSample);
         for (int j = 0; j < iNumChannels; ++j) {
             ppfOutputBuffer[j][i] = ppfInputBuffer[j][i] + mfGain * ppRingBuffer[j]->getPostInc();
             ppRingBuffer[j]->putPostInc(ppfOutputBuffer[j][i]);
--- src/MUSI6106Exec/MUSI6106Exec.cpp
+++ src/MUSI6106Exec/MUSI6106Exec.cpp
@@ -14,6 +14,13 @@ using std::endl;
 // local function declarations
 void    showClInfo ();
 
+int test_filtering(char inPath[], char outPath[], int block_length, int mode, float delay_length);
+
+int test_1();
+int test_2();
+int test_3();
+int test_4();
+
 /////////////////////////////////////////////////////////////////////////////////
 // main function
 int main(int argc, char* argv[])
@@ -38,9 +45,7 @@ int main(int argc, char* argv[])
     CAudioFileIf *phAudioFileOut = 0;
     std::fstream hTestOutputFile;
     CAudioFileIf::FileSpec_t stFileSpec;
-    //CRingBuffer<float>* pCRingBuff = 0;
-    //CCombFilterIf<float>* pCombFilter = 0;
-
+    
     showClInfo();
 
     //////////////////////////////////////////////////////////////////////////////
@@ -49,10 +54,24 @@ int main(int argc, char* argv[])
     {
         cout << "Missing audio input path!"<<endl;
         cout << "Running test mode!"<<endl;
-        sOutputFilePath = "test.txt";
-        testResult = 1;//run_test();
-        if(testResult==1)
-            cout<<"Test successfully"<<endl;
+        //sOutputFilePath = "test.txt";
+        char inputPath[50] =        "data/sin_400Hz.wav";
+        char inputPath_zero[50] =   "data/sin_400Hz.wav";
+        char outputPath_1[50] =     "data/sin_400Hz_out_1.wav";
+        char outputPath_2[50] =     "data/sin_400Hz_out_2.wav";
+        int  size[] = {2048,512,256,128};
+        
+        int testResult = 0;
+        
+        //test 1
+        testResult = testResult + test_filtering(inputPath, outputPath_1, 1024, 1, 0.00125);//400Hz half period
+        //test 2
+        testResult = testResult + test_filtering(inputPath, outputPath_2, 1024, 1, 0.0025);//400Hz
+        //test 3
+        
+        
+        if(testResult==2)
+            cout<<"Finish running tests! You may check the restored test output files"<<endl;
         else
             cout<<"Test failed"<<endl;
         return 0;
@@ -89,19 +108,7 @@ int main(int argc, char* argv[])
         CAudioFileIf::destroy(phAudioFileOut);
         return -1;
     }
-    //////////////////////////////////////////////////////////////////////////////
-    // open the output text file
-    /*
-    hTestOutputFile.open(sOutputFilePath.c_str(), std::ios::out);
-    if (!hTestOutputFile.is_open())
-    {
-        cout << "Text file open error!";
-        CAudioFileIf::destroy(phAudioFile);
-        return -1;
-    }
-    */
     
-    //////////////////////////////////////////////////////////////////////////////
     // allocate memory
     ppfAudioData = new float*[stFileSpec.iNumChannels];
     for (int i = 0; i < stFileSpec.iNumChannels; i++)
@@ -156,45 +163,8 @@ int main(int argc, char* argv[])
     
 
     cout << "\nreading/writing done in: \t" << (clock() - time) * 1.F / CLOCKS_PER_SEC << " seconds." << endl;
-    /*
-    ///////////////////////////
-    //Testing
-    //1/2. test simple square wave
-    float **ppfTestSignal = 0;
-    float **ppfTestSignalOut = 0;
     
-    ppfTestSignal = new float *[stFileSpec.iNumChannels];
-    // init to 0
-    for (int c = 0; c < stFileSpec.iNumChannels; c++) {
-        ppfTestSignal[c] = new float[20]();
-    }
-    // period = 10
-    int period = 10;
-    //int cnt = 0;
-    for (int cnt = 0; cnt < 20; cnt++){
-        if (cnt < period){
-            for (int c = 0; c < stFileSpec.iNumChannels; c++) {
-                ppfTestSignal[c][cnt] = 1.0;
-            }
-        }
-        else
-            for (int c = 0; c < stFileSpec.iNumChannels; c++) {
-                ppfTestSignal[c][cnt] = -1.0;
-            }
-    }
-    pCombFilterIf->process(ppfAudioData, ppfAudioDataOut, 20);
-    for (int i = 0; i < 20; i++)
-    {
-        for (int c = 0; c < stFileSpec.iNumChannels; c++)
-        {
-            hTestOutputFile << ppfAudioData[c][i] << "\t";
-        }
-        hTestOutputFile << endl;
-    }
-    //output observation: 1. should be 0s
     
-    //Test 3
-    */
     
     //////////////////////////////////////////////////////////////////////////////
     // clean-up (close files and free memory)
@@ -215,8 +185,123 @@ int main(int argc, char* argv[])
     // all done
     return 0;
 
-}
+}//main
 
+int test_filtering(char inPath[], char outPath[], int block_length, int mode, float delay_length)
+{
+    ///////////////////////////
+    std::string sInputFilePath,                 //!< file paths
+                sOutputFilePath,
+                sOutputAudPath;
+    int iTest = 0;
+    int kBlockSize;
+    //Testing
+    //1/2. test simple sine wave
+    sInputFilePath = inPath;
+    sOutputAudPath = outPath;
+    kBlockSize = block_length;
+    
+    //cout<<sOutputFilePath<<endl;
+    
+    float **ppfAudioData = 0;
+    float **ppfAudioDataOut = 0;
+    CCombFilterIf *pCombFilterIf=0;
+    
+    CAudioFileIf *phAudioFile = 0;
+    CAudioFileIf *phAudioFileOut = 0;
+    CAudioFileIf::FileSpec_t stFileSpec;
+    
+    float fDelayLength = delay_length;//400Hz
+    float fGain = 1;
+    float fMaxDelayLengthInS = 1;
+    
+    // open the input wave file
+    CAudioFileIf::create(phAudioFile);
+    phAudioFile->openFile(sInputFilePath, CAudioFileIf::kFileRead);
+    if (!phAudioFile->isOpen())
+    {
+        cout << "testWave file open error!";
+        CAudioFileIf::destroy(phAudioFile);
+        return -1;
+    }
+    phAudioFile->getFileSpec(stFileSpec);
+    
+    //output observation: 1. should be 0s
+    //create output file
+    CAudioFileIf::create(phAudioFileOut);
+    phAudioFileOut->openFile(sOutputAudPath, CAudioFileIf::kFileWrite, &stFileSpec);
+    if (!phAudioFileOut->isOpen())
+    {
+        cout << "testWave file open error!";
+        CAudioFileIf::destroy(phAudioFileOut);
+        return -1;
+    }
+    // allocate memory
+    ppfAudioData = new float*[stFileSpec.iNumChannels];
+    for (int i = 0; i < stFileSpec.iNumChannels; i++)
+        ppfAudioData[i] = new float[kBlockSize];
+    
+    ppfAudioDataOut = new float*[stFileSpec.iNumChannels];
+    for (int i = 0; i < stFileSpec.iNumChannels; i++)
+        ppfAudioDataOut[i] = new float[kBlockSize];
+    
+
+    if (ppfAudioData == 0)
+    {
+        CAudioFileIf::destroy(phAudioFile);
+        return -1;
+    }
+    if (ppfAudioDataOut[0] == 0)
+    {
+        CAudioFileIf::destroy(phAudioFileOut);
+        return -1;
+    }
+    
+    //////////////////////////////////////////////////////////////////////////////
+    //create filter object and decide which filter
+    CCombFilterIf::create(pCombFilterIf);
+    if(mode == 1)
+        pCombFilterIf->init(CCombFilterIf::kCombFIR, fDelayLength, stFileSpec.fSampleRateInHz, stFileSpec.iNumChannels);
+    else
+        pCombFilterIf->init(CCombFilterIf::kCombIIR, fDelayLength, stFileSpec.fSampleRateInHz, stFileSpec.iNumChannels);
+    pCombFilterIf->setParam(CCombFilterIf::kParamGain, fGain);
+    pCombFilterIf->setParam(CCombFilterIf::kParamDelay, fDelayLength);
+    
+    // get audio data, filter, and write it to the output
+    while (!phAudioFile->isEof())
+    {
+        // set block length variable
+        long long iNumFrames = kBlockSize;
+
+        // read data (iNumOfFrames might be updated!)
+        phAudioFile->readData(ppfAudioData, iNumFrames);
+        //cout << "\r" << "reading and filtering"<<endl;
+        // filter
+        pCombFilterIf->process(ppfAudioData, ppfAudioDataOut, iNumFrames);
+        //cout << "\r" << "Write audio"<<endl;
+        phAudioFileOut->writeData(ppfAudioDataOut, iNumFrames);
+        
+    }
+    cout << "\r" << "finish reading and filtering"<<endl;
+    
+    //////////////////////////////////////////////////////////////////////////////
+    // clean-up (close files and free memory)
+    phAudioFileOut->closeFile();
+    CAudioFileIf::destroy(phAudioFile);
+    CAudioFileIf::destroy(phAudioFileOut);
+
+    for (int i = 0; i < stFileSpec.iNumChannels; i++){
+        delete[] ppfAudioData[i];
+        delete[] ppfAudioDataOut[i];
+    }
+    delete[] ppfAudioData;
+    delete[] ppfAudioDataOut;
+    ppfAudioData = 0;
+    ppfAudioDataOut = 0;
+    
+    iTest = 1;
+    return iTest;
+}
 
 void     showClInfo()
 {
